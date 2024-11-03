d8a8501f9501d8d1ed8af0503287d5d1f7785444
Sweets Sweetman <sweetmantech@gmail.com>
2024-11-03 16:03:13 -0300

    I fixz stack type errors for ENV variables.

--- lib/stack/index.ts
+++ lib/stack/index.ts
@@ -1,6 +1,6 @@
 import { StackClient } from "@stackso/js-core";
 
 export const stack = new StackClient({
-  apiKey: process.env.STACK_API_KEY,
-  pointSystemId: process.env.STACK_POINT_SYSTEM_ID,
+  apiKey: process.env.STACK_API_KEY as string,
+  pointSystemId: parseInt(process.env.STACK_POINT_SYSTEM_ID as string),
 });
